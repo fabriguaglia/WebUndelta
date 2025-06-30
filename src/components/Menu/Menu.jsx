9969398a6555ebe02c34f4ba50f7c8fe16b9b823
fabriguaglia <fabriguaglia72@gmail.com>
2025-06-30 16:29:04 -0300

    menu fix

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import './Menu.css'
+import React from 'react';
+import './Menu.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import VideoUnd from "./UNDelta-inscripcion-carrerasABRIL-2025_v2-HORIZ.mp4"
+import VideoUnd from "./UNDelta-inscripcion-carrerasABRIL-2025_v2-HORIZ.mp4";
 
 function Menu() {
   return (
-    <div>
-        <video src={VideoUnd} class="object-fit-fill" autoplay></video>
+    <div className="video-container">
+      <video
+        src={VideoUnd}
+        autoPlay
+        muted
+        playsInline
+        loop
+        className="fullscreen-video"
+      ></video>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
