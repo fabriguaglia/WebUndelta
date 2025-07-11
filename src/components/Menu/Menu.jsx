ad7387cacaad53feca9eb8e045993b53a56519c7
Sanchez-Mariano <marianobensanchez@gmail.com>
2025-07-11 15:03:14 -0300

    cambios footer, nuevo carrousel

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Videodelta from "./UNDelta-inscripcion-carrerasABRIL-2025_v2-HORIZ.mp4";
 import './Menu.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Novedadesimg from './novedadesimage.png';
 
 function Menu() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const novedades = [
+    {
+      title: "Abrimos las inscripciones a nuestras carreras",
+      text: "Desde el 21 de abril, estarán abiertas las inscripciones a las primeras carreras de la Universidad Nacional del Delta.",
+      buttonText: "Conocé más",
+      image: Novedadesimg,
+      alt: "Inscripción UNDelta"
+    },
+    {
+      title: "Nuevas becas disponibles",
+      text: "La Universidad Nacional del Delta ofrece nuevas oportunidades de becas para estudiantes destacados. Postulate hasta el 15 de mayo.",
+      buttonText: "Ver más",
+      image: Novedadesimg,
+      alt: "Becas UNDelta"
+    },
+    {
+      title: "Jornada de puertas abiertas",
+      text: "Te invitamos a conocer nuestras instalaciones y a conversar con docentes y estudiantes. Próxima fecha: 30 de mayo.",
+      buttonText: "Inscribite",
+      image: Novedadesimg,
+      alt: "Puertas Abiertas UNDelta"
+    }
+  ];
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % novedades.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + novedades.length) % novedades.length);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentSlide(index);
+  };
   return (
     <>
       <div className="video-container">
@@ -88,37 +125,58 @@ function Menu() {
           <div className="col"></div>
         </div>
       </div>
+
       <div className="novedades-separator-custom">
         <div className="container-fluid">
           <h3 className="novedades-title-custom">NOVEDADES DESTACADAS</h3>
         </div>
       </div>
 
-      <div className="container-fluid info-slider-custom py-3">
-        <div className="row align-items-center justify-content-center gx-5">
-          <div className="col-md-6 text-center text-md-start mb-4 mb-md-0">
-            <h3 className="info-title-custom">Abrimos las inscripciones a nuestras carreras</h3>
-            <p className="info-text-custom">
-              Desde el 21 de abril, estarán abiertas las inscripciones a las primeras carreras de la Universidad Nacional del Delta.
-            </p>
-            <button className="info-button">Conocé más</button>
+      {/* Carousel React personalizado */}
+      <div className="carousel-custom-container">
+        <div className="container-fluid info-slider-custom py-3">
+          <div className="row align-items-center justify-content-center gx-5">
+            <div className="col-md-6 text-center text-md-start mb-4 mb-md-0">
+              <h3 className="info-title-custom">{novedades[currentSlide].title}</h3>
+              <p className="info-text-custom">
+                {novedades[currentSlide].text}
+              </p>
+              <button className="info-button">{novedades[currentSlide].buttonText}</button>
+            </div>
+            <div className="col-md-6 text-center">
+              <img
+                src={novedades[currentSlide].image}
+                alt={novedades[currentSlide].alt}
+                className="img-fluid info-image-custom"
+              />
+            </div>
           </div>
-          <div className="col-md-6 text-center">
-            <img
-              src={Novedadesimg}
-              alt="Inscripción UNDelta"
-              className="img-fluid info-image-custom"
-            />
+          
+          {/* Controles del carousel */}
+          <div className="carousel-controls text-center mt-4">
+            <button onClick={prevSlide} className="carousel-btn me-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#045976" className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+              </svg>
+            </button>
+            
+            {/* Indicadores */}
+            <div className="carousel-indicators-custom d-inline-block mx-3">
+              {novedades.map((_, index) => (
+                <button
+                  key={index}
+                  onClick={() => goToSlide(index)}
+                  className={`indicator-dot ${index === currentSlide ? 'active' : ''}`}
+                />
+              ))}
+            </div>
+            
+            <button onClick={nextSlide} className="carousel-btn ms-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#045976" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+              </svg>
+            </button>
           </div>
-          <div className="arrow-container text-end mt-3">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#045976" className="bi bi-arrow-left-circle me-2" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#045976" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-            </svg>
-          </div>
-
         </div>
       </div>
 
