9f660fdd0f2877eda2b368951139872e8439c9f0
fabriguaglia <fabriguaglia72@gmail.com>
2025-09-08 13:36:47 -0300

    google calendar api

--- src/components/Eventos/eventos.jsx
+++ src/components/Eventos/eventos.jsx
@@ -21,8 +21,16 @@ const Eventos = () => {
         {/* Calendar Container - Espacio reservado para el calendario */}
         <div className="calendario-container">
           <div className="calendario-placeholder">
-            {/* Aquí va tu componente de calendario */}
-            <p>En construcción.</p>
+            <iframe 
+                src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FArgentina%2FBuenos_Aires&showPrint=0&src=ZmFicmlndWFnbGlhNzJAZ21haWwuY29t&src=ZXMuYXIjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039be5&color=%230b8043" 
+                style={{
+                  border: "solid 1px #777"
+                }}
+                width="800" 
+                height="600" 
+                frameBorder="0" 
+                scrolling="no"
+              />
           </div>
         </div>
         </div>
